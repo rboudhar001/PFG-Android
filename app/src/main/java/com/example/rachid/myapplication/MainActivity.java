4468c26abae6c4aab1327a2533bb9e880077060e
Rachid <hiroshi.richi@gmail.com>
2016-03-23 22:59:07 +0100

    Tercer Commit

--- app/src/main/java/com/example/rachid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rachid/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rachid.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,13 +11,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // AÑADIDO
+        // -----------------------------------------------------------------------------------------
+        if (false) { // Si el usuario esta logeado
+            findViewById(R.id.frame_nav_header_main).setVisibility(View.VISIBLE);
+
+            // TODO: Mirar si existe (foto, nombre, email) del usuario en local, sino traerlo desde el servidor.
+            //findViewById(R.id.circle_image_profile).setBackground(/*Imagen tipo DRAWANBLE*/);
+            //findViewById(R.id.text_user_name).setBackground(/*Imagen tipo DRAWANBLE*/);
+            //findViewById(R.id.text_email_name).setBackground(/*Imagen tipo DRAWANBLE*/);
+        } else {
+            //FrameLayout frameNavHeaderMain = (FrameLayout) findViewById(R.id.frame_nav_header_main);
+            //frameNavHeaderMain.setVisibility(View.INVISIBLE);
+        }
+        // -----------------------------------------------------------------------------------------
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //AÑADIDO
         // ----------------------------------------------------------------------------------------
         //-----------------------------------------------------------------------------------------
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //AÑADIDO
@@ -105,5 +138,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.rachid.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.rachid.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
